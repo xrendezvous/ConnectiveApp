4ffb509295783c430b7d19016ac1d1c965fd72c5
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-06-10 01:33:02 +0300

    Update urls.py

--- app_main/urls.py
+++ app_main/urls.py
@@ -5,8 +5,5 @@ from . import views
 app_name = "app_main"
 
 urlpatterns = [
-    path("", views.main, name="root"),  # app_main:root
-    # path(
-    #     "app_main/about_application/", views.about_application, name="about_application"
-    # ),
+    path("", views.main, name="root"),
 ]
