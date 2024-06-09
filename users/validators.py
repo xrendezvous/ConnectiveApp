e91bed3b5d05af8e26ad16f6f722ec85d378b888
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-06-10 01:25:27 +0300

    Update validators.py

--- users/validators.py
+++ users/validators.py
@@ -3,7 +3,20 @@ from django.contrib.auth.models import User
 
 
 def validate_email(value):
+    """
+    Validate if the provided email is unique.
+    This function checks if the email already exists in the User model.
+
+    Args:
+    value (str): The email value to be validated.
+
+    Raises:
+    ValidationError: If a user with the provided email already exists.
+
+    Returns:
+    None
+    """
     if User.objects.filter(email=value).exists():
         raise ValidationError(
-            f"The user with {value} already exists.", params={"value": value}
+            f"Користувач з {value} вже існує.", params={"value": value}
         )
