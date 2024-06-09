8dfcd72bdec494987f1166d7b3286797c8aa9d8e
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-06-10 01:25:53 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,5 @@
-from django.urls import path, include
-from django.contrib.auth.views import (
-    LoginView,
-    LogoutView,
-    PasswordResetDoneView,
-    PasswordResetConfirmView,
-    PasswordResetCompleteView,
-)
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 from .forms import LoginForm
@@ -27,26 +21,5 @@ urlpatterns = [
         "signout/",
         LogoutView.as_view(template_name="users/signout.html"),
         name="signout",
-    ),
-    path("reset-password/", views.ResetPasswordView.as_view(), name="password_reset"),
-    path(
-        "reset-password/done/",
-        PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
-        name="password_reset_done",
-    ),
-    path(
-        "reset-password/confirm/<uidb64>/<token>/",
-        PasswordResetConfirmView.as_view(
-            template_name="users/password_reset_confirm.html",
-            success_url="/users/reset-password/complete/",
-        ),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset-password/complete/",
-        PasswordResetCompleteView.as_view(
-            template_name="users/password_reset_complete.html"
-        ),
-        name="password_reset_complete",
     ),
 ]
