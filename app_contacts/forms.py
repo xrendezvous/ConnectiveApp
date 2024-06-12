4f3043f75c5bf1d79236ebd2d002ca517d1a4b06
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-06-13 02:25:39 +0300

    Update forms.py

--- app_contacts/forms.py
+++ app_contacts/forms.py
@@ -1,46 +1,53 @@
 from django.core.validators import RegexValidator
 from django.forms import ModelForm, CharField, TextInput, DateInput, BooleanField, CheckboxInput, DateField, EmailField
-
+from .validators import validate_phone_number
 from .models import Contact, Address
 
 
 class ContactForm(ModelForm):
     name = CharField(min_length=3, max_length=20, required=True,
-                     widget=TextInput(attrs={'placeholder': "*Ім'я"}))
+                     widget=TextInput(attrs={'placeholder': "*Ім'я"}),
+                     error_messages={
+                         'required': "Це поле обов'язкове.",
+                         'min_length': "Ім'я повинно містити щонайменше 3 символи.",
+                         'max_length': "Ім'я повинно містити не більше 20 символів."
+                     }
+                     )
     surname = CharField(min_length=3, max_length=20, required=False,
-                        widget=TextInput(attrs={'placeholder': 'Прізвище'}))
+                        widget=TextInput(attrs={'placeholder': 'Прізвище'}),
+                        error_messages={
+                            'min_length': "Прізвище повинно містити щонайменше 3 символи.",
+                            'max_length': "Прізвище повинно містити не більше 20 символів."
+                        }
+                        )
     email = EmailField(required=False,
-                      widget=TextInput(attrs={'placeholder': 'Email'}))
-    mobile_phone = CharField(required=False, validators=[RegexValidator(
-        regex=r'\+?\d{1,3}\(?\d{2,4}\)?\d{3}(?:-?\d{3,4})(\d{3})?',
-        message='Номер телефону необхідно вводити у форматі: '
-                '+Код країни (код оператора)номер телефону Приклад: +380(50)1234567. Допускається до 15 цифр.'
-    )],
+                      widget=TextInput(attrs={'placeholder': 'Email'}),
+                       error_messages={
+                           'invalid': "Введіть правильний email адрес."
+                       }
+                       )
+    mobile_phone = CharField(required=False, validators=[validate_phone_number],
                              widget=TextInput(attrs={'placeholder': 'Номер телефону', "class": "form-control"}))
-    work_phone = CharField(required=False, validators=[RegexValidator(
-        regex=r'\+?\d{1,3}\(?\d{2,4}\)?\d{3}(?:-?\d{3,4})(\d{3})?',
-        message='Номер телефону необхідно вводити у форматі: '
-                '+Код країни (код оператора)номер телефону Приклад: +380(50)1234567. Допускається до 15 цифр.'
-    )],
+    work_phone = CharField(required=False, validators=[validate_phone_number],
                            widget=TextInput(attrs={'placeholder': 'Робочий телефон', "class": "form-control"}))
-    home_phone = CharField(required=False, validators=[RegexValidator(
-        regex=r'\+?\d{1,3}\(?\d{2,4}\)?\d{3}(?:-?\d{3,4})(\d{3})?',
-        message='Номер телефону необхідно вводити у форматі: '
-                '+Код країни (код оператора)номер телефону Приклад: +380(50)1234567. Допускається до 15 цифр.'
-    )],
+    home_phone = CharField(required=False, validators=[validate_phone_number],
                            widget=TextInput(attrs={'placeholder': 'Домашній телефон', "class": "form-control"}))
-    birthdate = DateField(required=False, input_formats=['%d/%m/%Y'], widget=DateInput(format='%d/%m/%Y'))
+    birthdate = DateField(required=False, input_formats=['%d/%m/%Y'], widget=DateInput(format='%d/%m/%Y'),
+                          error_messages={
+                              'invalid': "Введіть правильну дату у форматі ДД/ММ/РРРР."
+                          }
+                          )
     facebook = CharField(required=False, validators=[RegexValidator(
         regex=r'^https?://(www\.)?facebook\.com/.+',
-        message='Enter a valid Facebook URL.',
+        message='Введіть правильну URL адресу Facebook.',
     )], widget=TextInput(attrs={'placeholder': 'https://facebook.com', "class": "form-control"}))
     instagram = CharField(required=False, validators=[RegexValidator(
         regex=r'^https?://(www\.)?instagram\.com/.+',
-        message='Enter a valid Instagram URL.',
+        message='Введіть правильну URL адресу Instagram.',
     )], widget=TextInput(attrs={"placeholder": 'https://instagram.com', "class": "form-control"}))
     tiktok = CharField(required=False, validators=[RegexValidator(
         regex=r'^https?://(www\.)?tiktok\.com/.+',
-        message='Enter a valid Tiktok URL.',
+        message='Введіть правильну URL адресу Tiktok.',
     )], widget=TextInput(attrs={"placeholder": 'https://tiktok.com', "class": "form-control"}))
     is_favorite = BooleanField(required=False, label='Is favorite',
                                widget=CheckboxInput(attrs={'placeholder': 'is_favorite', "class": "form-check-input"}))
